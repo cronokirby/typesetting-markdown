03624f7f08ce8ce4e02b4eadcc5715b0f5dcafb7
Lúcás Meier <cronokirby@gmail.com>
2020-10-14 15:15:09 +0200

    Add (optional) colored logging

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from enum import Enum
 import logging
 from shutil import which
 
-
 REQUIREMENTS = [
     ('pandoc', 'https://www.pandoc.org'),
     ('context', 'https://wiki.contextgarden.net'),
@@ -12,11 +11,45 @@ REQUIREMENTS = [
 ]
 
 
-def setup_logger(verbose):
+class ColoredFormatter(logging.Formatter):
+
+    GRAY = "\x1b[38;1m"
+    BLUE = "\x1b[34;1m"
+    YELLOW = "\x1b[33;1m"
+    RED = "\x1b[31;1m"
+    BOLD = "\x1b[4m"
+    RESET = "\x1b[0m"
+
+    LEVEL_FORMAT = '[%(levelname)-7s]'
+    MESSAGE_FORMAT = '%(message)s'
+
+    FORMATS = {
+        logging.INFO: f'{GRAY}{BOLD}{LEVEL_FORMAT}{RESET} {GRAY}{MESSAGE_FORMAT}{RESET}',
+        logging.DEBUG: f'{BLUE}{BOLD}{LEVEL_FORMAT}{RESET} {BLUE}{MESSAGE_FORMAT}{RESET}',
+        logging.WARN: f'{YELLOW}{BOLD}{LEVEL_FORMAT}{RESET} {YELLOW}{MESSAGE_FORMAT}{RESET}',
+        logging.CRITICAL: f'{YELLOW}{BOLD}{LEVEL_FORMAT}{RESET} {YELLOW}{MESSAGE_FORMAT}{RESET}',
+        logging.ERROR: f'{RED}{BOLD}{LEVEL_FORMAT}{RESET} {RED}{MESSAGE_FORMAT}{RESET}',
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def setup_logger(verbose, no_color):
     level = logging.WARN
     if verbose:
         level = logging.INFO
-    logging.basicConfig(level=level, format='%(levelname)-8s %(message)s')
+
+    ch = logging.StreamHandler()
+    if no_color:
+        ch.setFormatter(logging.Formatter("[%(levelname)-7s] %(message)s"))
+    else:
+        ch.setFormatter(ColoredFormatter())
+    get_logger().addHandler(ch)
+    get_logger().propagate = False
+    logging.basicConfig(level=level)
 
 
 def get_logger():
@@ -40,8 +73,10 @@ def parse_args():
     Parse the arguments we need for our script.
     '''
     parser = argparse.ArgumentParser(description='Do stuff')
-    parser.add_argument('-v', '--verbose', action="store_true",
+    parser.add_argument('-v', '--verbose', action='store_true',
                         help='Log more information during execution')
+    parser.add_argument('--no-color', action='store_true',
+                        help='Do not use color when logging')
     return parser.parse_args()
 
 
@@ -67,11 +102,13 @@ def main():
     '''
     args = parse_args()
 
-    setup_logger(args.verbose)
+    setup_logger(args.verbose, args.no_color)
 
     if not check_requirements():
         exit(-1)
 
+    log_warn('Something bad has happened')
+    log_error('Something even worse has happened')
     log_info('Program requirements satisfied!')
 
 
